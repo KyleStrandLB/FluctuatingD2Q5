382abf6234b4d07885112708c6c842743b673d61
kyletstrand <kyletstrand@gmail.com>
2016-07-12 22:13:34 -0500

    Updates for improved running speed.

--- d2q5.c
+++ d2q5.c
@@ -31,80 +31,101 @@
 #define ydim 100                                   // Number of y lattice points
 
 double f[5][xdim][ydim], n[xdim][ydim]; 
-double noise[5], tau[5]={1,0.6,0.6,0.6,0.6};
-double n0[2]={30,30}, T0 = 1./3.;
-int next = 0, Pause = 1, done = 0, repeat = 1, iterations, writedata = 0, interval=1, intervalcnt=0;
-char filename[500]="data.dat";
+double tau[5]={1,0.6,0.6,0.6,0.6};
+double n0[2]={120,120}, theta = 1./3.;
+int next = 0, Pause = 0, done = 0, repeat = 1, iterations;
+
+//Haloing routine
+void Halo() {
+
+  for (int y=0;y<ydim;y++) {      // periodic boundary conditions
+    f[1][0][y]=f[1][xdim-2][y];
+    f[2][xdim-1][y]=f[2][1][y];
+  }
+  for (int x=0;x<xdim;x++) {
+    f[3][x][0]=f[3][x][ydim-2];
+    f[4][x][ydim-1]=f[4][x][1];
+  }
+}
+
+//Streaming routine
+void Stream() {
+
+  memmove(&f[1][1][0],&f[1][0][0],(xdim-1)*ydim*sizeof(double));
+  memmove(&f[2][0][0],&f[2][1][0],(xdim-1)*ydim*sizeof(double));
+  memmove(&f[3][0][1],&f[3][0][0],(xdim*ydim-1)*sizeof(double));
+  memmove(&f[4][0][0],&f[4][0][1],(xdim*ydim-1)*sizeof(double));
+}
+
+//Noise Routine
+void Noise(double *M, int i, int j) {
+
+  double noise[5];
+
+  noise[0]=0;
+  for (int a=1; a<5; a++) {
+    noise[a]=sqrt(n[i][j]*12)*((double)rand()/RAND_MAX - 0.5);     //local density noise
+    M[a] = ((1.-1./tau[1]) * M[a] + 1./tau[1]*(sqrt(2 * tau[1] - 1.) * noise[a]));
+  }
+}
 
 void init() {                                       // Initializing Eq. Dists
 
   iterations = 0;
-  for (int i = 1; i < xdim-1; i++) {
-    for (int j = 1; j < ydim-1; j++) {                  
+  for (int i = 0; i < xdim; i++) {
+    for (int j = 0; j < ydim; j++) {                  
       n[i][j]=n0[0];
-      f[0][i][j] = n[i][j] * (1 - 2*T0);
-      for (int a=1; a<5; a++) f[a][i][j]=n[i][j]/2. * T0;
+      f[0][i][j] = n[i][j] * (1 - 2*theta);
+      for (int a=1; a<5; a++) f[a][i][j]=n[i][j]/2. * theta;
     }
   }
 }
 
-void iteration() {                                  // Iteration step
+//void iteration(double  m[5][5]) {                                  // Iteration step
+void iteration() {
   
   double M[5];                              
 
+  //It seems to be slightly faster to declare this matrix each iteration rather than declare it in the main function
   double m[5][5] = {
-                     {1.,1.,1.,1.,1.},
-                     {0.,sqrt(1./T0),-sqrt(1./T0),0.,0.},
-                     {0,0,0,sqrt(1./T0),-sqrt(1./T0)},
-                     {0,sqrt(1./(2.*T0)),sqrt(1./(2.*T0)),-sqrt(1./(2.*T0)),-sqrt(1./(2.*T0))},
-                     {-sqrt(2.*T0/(1.-2.*T0)),sqrt((1.-2.*T0)/(2.*T0)),sqrt((1.-2.*T0)/(2.*T0)),sqrt((1.-2.*T0)/(2.*T0)),sqrt((1.-2.*T0)/(2.*T0))}
-                   };                                   // Transformation matrix
+    {1.,1.,1.,1.,1.},
+    {0.,sqrt(1./theta),-sqrt(1./theta),0.,0.},
+    {0,0,0,sqrt(1./theta),-sqrt(1./theta)},
+    {0,sqrt(1./(2.*theta)),sqrt(1./(2.*theta)),-sqrt(1./(2.*theta)),-sqrt(1./(2.*theta))},
+    {-sqrt(2.*theta/(1.-2.*theta)),sqrt((1.-2.*theta)/(2.*theta)),sqrt((1.-2.*theta)/(2.*theta)),sqrt((1.-2.*theta)/(2.*theta)),sqrt((1.-2.*theta)/(2.*theta))}
+  };                                   // Transformation matrix*/
 
-  for (int i = 1; i < xdim-1; i++) {                                     
-      for (int j = 1; j < ydim-1; j++) {
+  for (int i = 0; i < xdim; i++) {                                     
+      for (int j = 0; j < ydim; j++) {
         n[i][j] = f[0][i][j] + f[1][i][j] + f[2][i][j] + f[3][i][j] + f[4][i][j];                   // Sum of Eq dists = density
 
         for (int a=0; a<5; a++) M[a]=0;
 
+        // Forward transformation
         for (int a=0; a<5; a++) {
-          for (int b=0; b<5; b++) {         //Forward transformation
-            M[a] += m[a][b] * f[b][i][j];   // the matrix multiplication was backwards before i think
+          for (int b=0; b<5; b++) {       
+            M[a] += m[a][b] * f[b][i][j];   
           }
         }
 
-      noise[0]=0;
-      for (int a=1; a<5; a++) noise[a]=sqrt(n[i][j]*12)*((double)rand()/RAND_MAX - 0.5);     //local density noise
-
-      M[1] = ((1.-1./tau[1]) * M[1] + 1./tau[1]*(sqrt(2 * tau[1] - 1.) * noise[1]));  
-      M[2] = ((1.-1./tau[2]) * M[2] + 1./tau[2]*(sqrt(2 * tau[2] - 1.) * noise[2]));
-      M[3] = ((1.-1./tau[3]) * M[3] + 1./tau[3]*(sqrt(2 * tau[3] - 1.) * noise[3]));
-      M[4] = ((1.-1./tau[4]) * M[4] + 1./tau[4]*(sqrt(2 * tau[4] - 1.) * noise[4]));
+      // call noise routine to add noise to moment space functions
+      Noise(M, i, j);
 
       f[0][i][j] = f[1][i][j] = f[2][i][j] = f[3][i][j] = f[4][i][j] = 0;
 
-      for (int a=0; a<5; a++) {          // Back transform
+      // Back transform
+      for (int a=0; a<5; a++) {       
           for (int u=0; u<5; u++) {
-            if (a == 0) f[a][i][j] += m[u][a] *(1-2*T0)*M[u];                      //m[b][a] * M[b]
-            else f[a][i][j] += m[u][a] * (T0/2) *M[u];
+            if (a == 0) f[a][i][j] += m[u][a] *(1-2*theta)*M[u];                      
+            else f[a][i][j] += m[u][a] * (theta/2) *M[u];
           }
       }
     }
   } 
 
-    for (int y=0;y<ydim;y++) {      // periodic boundary conditions
-      f[1][0][y]=f[1][xdim-2][y];
-      f[2][xdim-1][y]=f[2][1][y];
-    }
-    for (int x=0;x<xdim;x++) {
-      f[3][x][0]=f[3][x][ydim-2];
-      f[4][x][ydim-1]=f[4][x][1];
-    }
-
-      memmove(&f[1][1][0],&f[1][0][0],(xdim-1)*ydim*sizeof(double));       //Check this
-      memmove(&f[2][0][0],&f[2][1][0],(xdim-1)*ydim*sizeof(double));
-      memmove(&f[3][0][1],&f[3][0][0],(xdim*ydim-1)*sizeof(double));
-      memmove(&f[4][0][0],&f[4][0][1],(xdim*ydim-1)*sizeof(double));        // i had paraenths on xdim*(ydim-1)
-
+  //Call halo and streaming routines
+  Halo();
+  Stream();
   iterations++;
 }
 
@@ -124,7 +145,7 @@ void GUI() {
   StartMenu("Parameters",0);
     DefineDouble("n0_x",&n0[0]);
     DefineDouble("n0_y",&n0[1]);
-    DefineDouble("T0",&T0);
+    DefineDouble("Theta",&theta);
     StartMenu("Relaxation Times",0);
       DefineDouble("Tau_0",&tau[0]);
       DefineDouble("Tau_1",&tau[1]);
